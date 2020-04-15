32bee6960a214f9439d7900ce30d120642626142
Justin Ochoa <just.och92@gmail.com>
2020-04-15 17:12:59 -0400

    2nd draft

--- index.js
+++ index.js
@@ -1,60 +1,97 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var startBtn=document.getElementById('startBtn');
 canvas.width = 600;
 canvas.height = 500;
+let score = 0;
+let lives = 3;
 
+// function gameStart(){
+//   startBtn.style.display="none";
+// }
 //define the alien invaders
+
+var alienImage= new Image();
+alienImage.src= './images/alien (1).png';
+alienImage.onload=  function (e){
+  drawAlien();
+};
+
 function drawAlien() {
   ctx.fillStyle = "red";
   aliens.forEach((alien) => {
-    ctx.fillRect(alien.x, alien.y, alien.w, alien.h); //optional chaining
+    let x = alien.x;
+    if (x < alien.destinationX) {
+      alien.x += 5;
+    }
+    ctx.drawImage(alienImage,alien.x, alien.y, alien.w, alien.h);
+  });
+  
+}
+function drawAliensBullets() {
+  ctx.fillStyle = "red";
+  aliensBullets.forEach((aBull, j) => {
+    if (aBull.y > canvas.height) {
+      aliensBullets.splice(j, 1); //get rids of bullet when past screen
+    }
+    ctx.fillRect(aBull.x, (aBull.y +=4), aBull.w, aBull.h); //optional chaining
   });
   //ctx.fillRect(aliens[i]?.x,aliens[i]?.y,aliens[i]?.w,aliens[i]?.h);//optional chaining
 }
-function drawAliensBullets() {
-    ctx.fillStyle = "red";
-    aliensBullets.forEach((aBull,j) => {
-        if(aBull.y>canvas.height){
-            aliensBullets.splice(j,1) //get rids of bullet when past screen
-        }
-      ctx.fillRect(aBull.x, aBull.y+=3, aBull.w, aBull.h); //optional chaining
-    });
-    //ctx.fillRect(aliens[i]?.x,aliens[i]?.y,aliens[i]?.w,aliens[i]?.h);//optional chaining
-  }
 let aliens = [];
-let aliensBullets=[];
+let aliensBullets = [];
 setInterval(function () {
-let x = Math.random() * canvas.width
+  let x = Math.random() * canvas.width;
   let alien = {
-    x: x,
+    x: -10,
     y: 100,
-    w: 20,
-    h: 20,
-    startShooting:() => {
-        let bullet= {x: x,y:100, w:3, h:7 }
-        setInterval(() => {
-            aliensBullets.push(bullet)
-        
-        }, 1000)
-    }
+    w: 65,
+    h: 65,
+    destinationX: x,
+    destinationY: 100,
+
+    get startShooting() {
+      return setInterval(() => {
+        console.log('Create a bullet');// creating bullet every 3 seconds
+
+        let bullet = { x: this.x + this.w / 2, y: this.y, w: 3, h: 7 };
+        aliensBullets.push(bullet);
+      }, 3000);
+    },
   };
-  alien.startShooting()
+  //alien.startShooting;
   aliens.push(alien);
-}, 1000);
-//draw the shooter
-function drawShooter() {
-  ctx.fillStyle = "blue";
-  ctx.fillRect(shooter.x, shooter.y, shooter.w, shooter.h);
+}, 3000);
+setInterval(() => {
+  console.log('did it work');// creating bullet every 3 seconds
+if(aliens.length !==0 ){
+for(let i=0; i<aliens.length; i++){
+  console.log(aliens[i]);
+  let bullet = { x: aliens[i].x + aliens[i].w / 2, y: aliens[i].y, w: 3, h: 7 };
+    aliensBullets.push(bullet)
 }
-let shooter = {
-  x: canvas.width / 2,
-  y: 480,
-  w: 20,
-  h: 50,
-  color: "blue",
+}
+},3000);
+
+// //draw the shooter
+var shooterImage= new Image();
+shooterImage.src= './images/enterprise copy.png';
+shooterImage.onload=  function (e){
+  drawShooter();
 };
 
-//move the shooter along a line
+let shooter = {
+  x: canvas.width / 2,
+  y: 420,
+  w: 70,
+  h: 70,
+};
+function drawShooter() {
+  ctx.drawImage(shooterImage,shooter.x, shooter.y,shooter.w,shooter.h);
+}
+
+
+//move the shooter a long a line
 
 document.body.onkeydown = checkKey;
 function checkKey(e) {
@@ -83,24 +120,30 @@ function detectCollision() {
       ) {
         aliens.splice(a, 1);
         bullets.splice(i, 1);
+        score++;
+        addScore();
       }
     });
   });
 }
+// detect shots from alien to me
 function detectShooterCollision() {
-    aliensBullets.forEach((aBull, a) => {
-     
-        if (
-          aBull.x < shooter.x + shooter.w &&
-          aBull.x + aBull.w > shooter.x &&
-          aBull.y < shooter.y + shooter.h &&
-          aBull.y + aBull.h > shooter.y
-        ) {
-          aliensBullets.splice(a, 1);
-          window.cancelAnimationFrame(animationId)
-        }
-    });
-  }
+  aliensBullets.forEach((aBull, a) => {
+    if (
+      aBull.x < shooter.x + shooter.w &&
+      aBull.x + aBull.w > shooter.x &&
+      aBull.y < shooter.y + shooter.h &&
+      aBull.y + aBull.h > shooter.y
+    ) {
+      aliensBullets.splice(a, 1);
+      //window.cancelAnimationFrame(animationId);
+      lives--;
+      gameOver();
+    }
+    // document.location.reload();
+    //     clearInterval();
+  });
+}
 //shoot aliens
 
 let bullets = [];
@@ -115,11 +158,29 @@ function shoot() {
 }
 
 function drawBullets() {
-  ctx.fillStyle = "white";
+  ctx.fillStyle = "green";
   bullets.forEach((bullet) => {
     ctx.fillRect(bullet.x, (bullet.y -= 15), bullet.w, bullet.h);
   });
 }
+//Adding score everytime alien is hit.
+function addScore() {
+  document.querySelector( "#result").innerText = `Score : ${score}`;
+}
+ function gameOver(){
+    if(score===3){
+    alert ("You Won!");
+      //document.location.reload();
+      cancelAnimationFrame(animationId);
+    }
+ else {
+      if(lives===0){
+        alert("You Lose!");
+
+        cancelAnimationFrame(animationId);
+        }
+        document.querySelector( "#life").innerText =`Lives : ${lives}`;}
+  }
 
 let animationId = null;
 function animationLoop() {
@@ -131,5 +192,7 @@ function animationLoop() {
   detectCollision();
   drawAliensBullets();
   detectShooterCollision();
+  addScore();
+   gameOver();
 }
 animationLoop();
